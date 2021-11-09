e2a557f1129e2befd6a85b9a3df053d6dca91b37
Nadyan <nadyan_sp@hotmail.com>
2021-11-09 20:35:15 -0300

    rotas

--- api/index.js
+++ api/index.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
-
-app.use(bodyparser.json());
-
-app.get('/teste', (req, res) => {
-    res.status(200).send({mensagem: 'Hellaaaaw daawwwg!'});
-});
-
 const port = 3000;
+
+routes(app);
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 });
