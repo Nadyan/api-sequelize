3c02baed23d91e4876dbc65383bdbd8cdf510cf6
Nadyan <nadyan_sp@hotmail.com>
2021-11-10 11:44:22 -0300

    verbos get e post

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -10,6 +10,33 @@ class PessoaController {
             return res.status(500).json(err.message);
         }
     }
+
+    static async selecionaPessoa(req, res) {
+        const { id } = req.params;
+        try {
+            const dados = await database.Pessoas.findOne(
+                { 
+                    where: { 
+                        id: Number(id)
+                    }
+                }
+            );
+            return res.status(200).json(dados);
+        } catch (err) {
+            return res.status(500).json(err.message);
+        }
+    }
+
+    static async criaPessoa(req, res) {
+        const dados = req.body;
+
+        try {
+            const { id } = await database.Pessoas.create(dados);
+            return res.status(200).json(id);
+        } catch (err) {
+            return res.status(500).json(err.message);
+        }
+    }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,7 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.selecionaTudo);
+router.get('/pessoas/:id', PessoaController.selecionaPessoa);
+router.post('/pessoas', PessoaController.criaPessoa);
 
 module.exports = router;
